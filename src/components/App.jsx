88f9d06344af95614a2f33b8a1e73fa870296c08
Pavlo <pasam9300@gmail.com>
2022-11-20 17:40:56 +0100

    Done HM

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { fetchImages } from 'helpers/pixabayApi';
 import React, { Component } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { LoadMore } from './LoadMore/LoadMore';
+import { Loader } from './Loader/Loader';
 import { SearchBar } from './Searchbar/SearchBar';
 
 export class App extends Component {
@@ -10,6 +11,7 @@ export class App extends Component {
     query: '',
     page: 1,
     totalHits: 0,
+    isLoading: false,
   };
 
   handleSubmitForm = query => {
@@ -22,16 +24,22 @@ export class App extends Component {
 
   componentDidUpdate(_, prevState) {
     const { query, page } = this.state;
-    if (prevState !== query || prevState.page !== page) {
-      fetchImages(query, page).then(data => {
-        this.setState(prev => ({
-          images: page === 1 ? data.hits : [...prev.images, ...data.hits],
-          totalHits:
-            page === 1
-              ? data.totalHits - data.hits.length
-              : data.totalHits - [...prev.images, ...data.hits].length,
-        }));
-      });
+    if (prevState.query !== query || prevState.page !== page) {
+      this.setState({ isLoading: true });
+      fetchImages(query, page)
+        .then(data => {
+          this.setState(prev => ({
+            images: page === 1 ? data.hits : [...prev.images, ...data.hits],
+
+            totalHits:
+              page === 1
+                ? data.totalHits - data.hits.length
+                : data.totalHits - [...prev.images, ...data.hits].length,
+          }));
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
     }
   }
 
@@ -40,9 +48,13 @@ export class App extends Component {
       <>
         <SearchBar onSubmit={this.handleSubmitForm} />
         <ImageGallery images={this.state.images} />
-        {!!this.state.totalHits && (
-          <LoadMore onLoadMore={this.handleLoadMore} />
-        )}
+
+        {!!this.state.totalHits &&
+          (!this.state.isLoading ? (
+            <LoadMore onLoadMore={this.handleLoadMore} />
+          ) : (
+            <Loader />
+          ))}
       </>
     );
   }
--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
+
 export const LoadMore = ({ onLoadMore }) => {
   return (
     <button onClick={onLoadMore} type="button">
       Load More
     </button>
   );
+};
+
+LoadMore.propTypes = {
+  onLoadMore: PropTypes.func,
 };
